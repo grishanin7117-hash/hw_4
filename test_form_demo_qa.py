c3bcf17ae098e1906970a97970a4d6a492c09e31
Nikita Grishanin <grishanin7117@gmail.com>
2025-12-17 15:53:21 +0300

    путь

--- test_form_demo_qa.py
+++ test_form_demo_qa.py
@@ -1,43 +1,63 @@
-from selene import browser, have
-from pathlib import Path
+import os
+from selene import browser, have, by, be
+from selene.support.conditions import be as sbe
 
+# Функция для получения абсолютного пути к картинке КАК В ВАШЕМ ПРИМЕРЕ
+def get_image_path(image_name):
+    # Получаем абсолютный путь к картинке ТОЧНО КАК У ВАС
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # Только добавляем папку 'images' перед именем файла
+    return os.path.join(current_dir, 'images', image_name)
 
-def test ():
+# Тест с вашими данными
+def test():
     browser.open('https://demoqa.com/automation-practice-form')
-    browser.element('#firstName').type( 'Игорь')
+
+    browser.driver.execute_script("$('#fixedban').remove()")
+    browser.driver.execute_script("$('footer').remove()")
+
+    browser.element('#firstName').type('Игорь')
     browser.element('#lastName').type('Киров')
     browser.element('#userEmail').type('qwerty@mail.com')
     browser.element('label[for="gender-radio-1"]').click()
     browser.element('#userNumber').type('8167873187')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('#subjectsInput').type('commerce').press_enter()
-    browser.element('.react-datepicker__month-select').click()
-    browser.element('.react-datepicker__month-select option[value ="6"]').click()
-    browser.element('.react-datepicker__year-select').click()
-    browser.element('.react-datepicker__year-select option[value ="1944"]').click()
-    browser.element('[aria-label="Choose Tuesday, July 4th, 1944"]').click()
-    picture_path = str(Path.home().joinpath('Pycharmprojects', 'hw_4', 'images', 'qa11.jpg.jpg').resolve())
-    browser.element('#uploadPicture').set_value(picture_path)
-    browser.element('label[for="hobbies-checkbox-1"]').click()
-    browser.element('#currentAddress').type('red polyna').press_enter()
-    browser.execute_script("arguments[0].click();", browser.element('#state').locate())
-    browser.element('#react-select-3-input').type('Rajasthan').press_enter()
-    browser.element('#city').click()
-    browser.element('#react-select-4-input').type('Jaipur').press_enter()
-    browser.element('#submit').click()
-    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
-    browser.all('.table td:nth-child(2)').should(have.exact_texts(
-        'Игорь Киров',
-        'qwerty@mail.com',
-        'Male',
-        '8167873187',
-        '04 July,1944',
-        'Commerce',
-        'Sports',
-        'qa11.jpg.jpg',
-        'red polyna',
-        'Rajasthan Jaipur'
-    ))
 
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('.react-datepicker__year-select').element(by.text("1944")).click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('.react-datepicker__month-select').element(by.text("July")).click()
+    browser.element('.react-datepicker__day--004').click()
+
+    browser.element('#subjectsInput').type('commerce').press_enter()
+
+    # Загрузка изображения ТОЧНО КАК В ВАШЕМ ПРИМЕРЕ
+    image_path = get_image_path('qa11.jpg')
+    browser.element('#uploadPicture').send_keys(image_path)
+
+    browser.element('label[for="hobbies-checkbox-1"]').click()
+
+    browser.element('#currentAddress').type('red polyna')
+
+    browser.element('#state').click()
+    browser.element('#state').element(by.text("Rajasthan")).click()
+    browser.element('#city').click()
+    browser.element('#city').element(by.text("Jaipur")).click()
+
+    browser.element('#submit').click()
+
+    browser.element('.modal-dialog').should(sbe.visible)
+
+    browser.element('.modal-body').should(have.text('Игорь'))
+    browser.element('.modal-body').should(have.text('Киров'))
+    browser.element('.modal-body').should(have.text('qwerty@mail.com'))
+    browser.element('.modal-body').should(have.text('8167873187'))
+    browser.element('.modal-body').should(have.text('04 July,1944'))
+    browser.element('.modal-body').should(have.text('Commerce'))
+    browser.element('.modal-body').should(have.text('Sports'))
+    browser.element('.modal-body').should(have.text('qa11.jpg'))
+    browser.element('.modal-body').should(have.text('red polyna'))
+    browser.element('.modal-body').should(have.text('Rajasthan'))
+    browser.element('.modal-body').should(have.text('Jaipur'))
 
 
